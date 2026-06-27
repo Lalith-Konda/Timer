d8baf639fa0a2e2f2646f83c35875ed2074023d0
Lalith-Konda <balasubrahmanyamkonda@gmail.com>
2026-06-27 17:56:01 +0530

    Refactor timer toggle functionality for user input

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
 import * as Timer from "./timer.js";
 import { updateDisplay } from "./ui.js";
 
-document.addEventListener("click", () => {
-
+function toggleTimer() {
     if (Timer.running()) {
         Timer.stop();
     } else {
         Timer.start(updateDisplay);
     }
+}
 
+// For desktops/laptops: spacebar
+document.addEventListener("keydown", (event) => {
+    if (event.code === "Space") {
+        event.preventDefault(); // prevent page scroll
+        toggleTimer();
+    }
+});
+
+// For mobiles: touch
+document.addEventListener("touchstart", () => {
+    toggleTimer();
 });
